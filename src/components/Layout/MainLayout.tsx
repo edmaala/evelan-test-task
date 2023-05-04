9529fa6fb492b3fcaf86a9803af77984b7448ca4
edmaala <edward.dilao.maala@gmail.com>
2023-05-05 03:47:30 +0800

    🐛 Enhance page accessibility

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -9,7 +9,7 @@ const MainLayout: React.FC<Props> = ({ children }) => {
         <title>User List | Evelan GmbH</title>
         <meta
           name='viewport'
-          content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes'
+          content='width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes'
         />
       </Head>
       <main className={classes.mainLayout}>{children}</main>
--- src/components/Typography/UserId.tsx
+++ src/components/Typography/UserId.tsx
@@ -3,7 +3,12 @@ import React, { ReactNode } from 'react';
 const UserId: React.FC<Props> = ({ children }) => {
   return (
     <span
-      style={{ fontSize: '0.8rem', color: 'var(--green)', lineHeight: '1.2em' }}
+      style={{
+        fontSize: '0.8rem',
+        color: 'var(--green)',
+        lineHeight: '1.2em',
+        fontWeight: '600',
+      }}
     >
       ID# {children}
     </span>
